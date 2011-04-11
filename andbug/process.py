d45edae424181080a7f1d501d06faf2ddab161d4
Scott Dunlop <swdunlop@gmail.com>
2011-04-11 00:55:02 -0700

    content and therefore requests can be written synchronously

--- andbug/process.py
+++ andbug/process.py
@@ -11,7 +11,7 @@
 ##       notice, this list of conditions and the following disclaimer in the
 ##       documentation and/or other materials provided with the distribution.
 ## 
-## THIS SOFTWARE IS PROVIDED BY SCOTT DUNLOP "AS IS" AND ANY EXPRESS OR 
+## THIS SOFTWARE IS PROVIDED BY SCOTT DUNLOP 'AS IS' AND ANY EXPRESS OR 
 ## IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
 ## OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
 ## IN NO EVENT SHALL SCOTT DUNLOP OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
@@ -67,6 +67,7 @@ class Process(Thread):
 		self.read = read
 		self.write = write
 		self.initialized = False
+		self.nextId = 3
 
 	###################################################### INITIALIZATION STEPS
 	
@@ -76,13 +77,13 @@ class Process(Thread):
 	def readIdSzRes(self):
 		head = self.readHeader();
 		if head[0] != 20:
-			raise ProtocolError("expected size of an idsize response")
+			raise ProtocolError('expected size of an idsize response')
 		if head[2] != 0x80:
-			raise ProtocolError("expected first server message to be a response")
+			raise ProtocolError('expected first server message to be a response')
 		if head[1] != 1:
-			raise ProtocolError("expected first server message to be 1")
+			raise ProtocolError('expected first server message to be 1')
 
-		sizes = self.recvbuf.unpack( "iiiii", self.readContent(20) )
+		sizes = self.recvbuf.unpack( 'iiiii', self.read(20) )
 		self.recvbuf.config(*sizes)
 		self.xmitbuf.config(*sizes)
 		return None
@@ -107,10 +108,37 @@ class Process(Thread):
 	def readContent(self, size):
 		return self.read(size)
 
+	####################################################### TRANSMITTING PACKETS
+	
+	def writeHeader(self, size, flags, event):
+		'used internally by the processor'
+
+		ident = self.nextId
+		self.nextId += 2		
+		size += 11
+		data = self.xmitbuf.pack(
+			HEADER_FORMAT, size, ident, flags, event
+		)
+		self.write(data)
+		return ident
+
+	def writeContent(self, content):
+		'sends a response or request'
+
+		code = content.code
+		flags = content.flags
+		self.xmitbuf.preparePack()
+		content.packTo(self.xmitbuf)
+		data = self.xmitbuf.data()
+		size = len(data)
+		self.writeHeader( size, flags, code )
+		return self.write(data)
+
 	################################################################# THREAD API
 	
 	def start(self):
-		"performs handshaking and solicits configuration information"
+		'performs handshaking and solicits configuration information'
+
 		if not self.initialized:
 			self.writeHandshake()
 			self.readHandshake()
@@ -125,3 +153,87 @@ class Process(Thread):
 		#TODO while True:
 		#TODO 	self.processPacket(*self.readPacket())
 	
+class Element(object):
+	'''
+	Elements, such as messages or entries found in messages, can be packed to
+	a JDWP buffer, or unpacked from them.
+	'''
+
+	def __init__(self):
+		pass
+
+	def packTo(self, buf):
+		'packs data associated with this element into the buffer'
+		pass
+
+	@classmethod
+	def unpackFrom(impl, buf):
+		'creates a new instance from the contents of the buffer'
+		return impl()
+		#TEST
+ 
+class Content(Element):
+	'''
+	A JDWP packet consists of a Header and an optional Content.  Descendants
+	of the Content class do not manage the Header portion -- the Processor will
+	derive this prior to unpackFrom and after packTo.
+	'''
+
+	def __init__(self):
+		Element.__init__(self)
+		
+	@property
+	def code(self):
+		'''
+		specifies either the content of the cmdset and cmd fields, or the 
+		response code
+		'''
+		return self.CODE
+
+	@property
+	def flags(self):
+		'returns the static FLAGS associated with this Content type'
+		return self.FLAGS
+
+class Response(Content):
+	'''
+	Expresses a response to a JDWP request; shared base of Failure and Success
+	'''
+	FLAGS = 0x80
+	pass #TODO
+
+class Failure(Response):
+	'''
+	Failures are error responses to a request; each individual Failure class
+	should specify its associated CODE, as specified in JVMDI.
+	'''
+	pass #TODO
+
+class Success(Response):
+	'''
+	Successes are responses to a request; since the Processor cannot determine
+	the correct class of a Success without contextual information about the
+	Request, each Request should have a associated unpackSuccess function.
+	'''
+	CODE = 0x0000
+
+class Request(Content):
+	'''
+	Requests made from the debugger to the process should have an associated
+	SUCCESS class that supports the unpackFrom class method.
+	'''
+	FLAGS = 0x00
+	SUCCESS = None
+
+	def unpackSuccessFrom(self, buf):
+		return self.SUCCESS.unpackFrom(buf)
+		#TEST
+
+class Event(Request):
+	'''
+	Events are a special case of Request, where the debugger recieves a 
+	"request" from the process.  Events are unusual, in that a number of
+	of them are collected into a single request, and that no response is
+	expected by the process from the debugger.
+	'''
+	pass #TODO
--- tests/process.py
+++ tests/process.py
@@ -1,4 +1,4 @@
-from andbug.process import Process, HANDSHAKE_MSG, IDSZ_REQ
+from andbug.process import Process, Success, Request, HANDSHAKE_MSG, IDSZ_REQ
 from unittest import TestCase, main as test_main
 from cStringIO import StringIO
 import sys
@@ -33,15 +33,48 @@ class IoHarness:
 		exp = self.writebuf.read(len(data))
 		self.test.assertEqual(exp, data)
 
+def make_proc(harness):
+	proc = Process(harness.read, harness.write)
+	proc.start()
+	return proc
+
+class SampleResponse(Success):
+	def __init__(self, val):
+		Success(self)
+		self.val = val
+
+	def unpackFrom(impl, buf):
+		return impl(buf.unpack16())
+	
+class SampleRequest(Request):
+	SUCCESS = SampleResponse
+	CODE = 0x4242
+
+SAMPLE_REQ = (
+	'\x00\x00\x00\x0B' # Length
+	'\x00\x00\x00\x03' # Identifier
+	'\x00'             # Request
+	'\x42\x42'         # Not an error
+)
+
 class TestProcess(TestCase):
 	def test_start(self):
 		h = IoHarness( self, [
 			(HANDSHAKE_MSG, HANDSHAKE_MSG),
 			(IDSZ_REQ, IDSZ_RES)
 		])
-		p = Process(h.read, h.write)
-		p.start()
+		p = make_proc(h)
 		self.assertEqual(True, p.initialized)
+
+	def test_pack(self):
+		h = IoHarness( self, [
+			(HANDSHAKE_MSG, HANDSHAKE_MSG),
+			(IDSZ_REQ, IDSZ_RES),
+			(SAMPLE_REQ, '')
+		])
+		p = make_proc(h)
+		req = SampleRequest()
+		p.writeContent(req)
 
 if __name__ == '__main__':
 	test_main()
