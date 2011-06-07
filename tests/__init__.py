4a577edd4cd50f3c80442cdcf995d2759c7d06b2
Scott Dunlop <swdunlop@gmail.com>
2011-06-07 11:34:14 -0700

    moved to the LGPL so AndBug won't get silently inserted into IDA et al.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,2 +1,15 @@
-import jdwp
+## Copyright 2011, Scott W. Dunlop <swdunlop@gmail.com> All rights reserved.
+##
+## AndBug is free software: you can redistribute it and/or modify it under 
+## the terms of version 3 of the GNU Lesser General Public License as 
+## published by the Free Software Foundation.
+##
+## AndBug is distributed in the hope that it will be useful, but WITHOUT ANY
+## WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+## FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for 
+## more details.
+##
+## You should have received a copy of the GNU Lesser General Public License
+## along with AndBug.  If not, see <http://www.gnu.org/licenses/>.
 
+import jdwp
--- tests/log.py
+++ tests/log.py
@@ -1,3 +1,17 @@
+## Copyright 2011, Scott W. Dunlop <swdunlop@gmail.com> All rights reserved.
+##
+## AndBug is free software: you can redistribute it and/or modify it under 
+## the terms of version 3 of the GNU Lesser General Public License as 
+## published by the Free Software Foundation.
+##
+## AndBug is distributed in the hope that it will be useful, but WITHOUT ANY
+## WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+## FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for 
+## more details.
+##
+## You should have received a copy of the GNU Lesser General Public License
+## along with AndBug.  If not, see <http://www.gnu.org/licenses/>.
+
 from andbug.log import LogReader, LogWriter, LogEvent
 from unittest import TestCase, main as test_main
 from cStringIO import StringIO
