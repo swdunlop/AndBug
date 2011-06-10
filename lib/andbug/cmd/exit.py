c2c82b22a0e37a75deb1622bbf784eb38dbd2fb1
Scott Dunlop <swdunlop@gmail.com>
2011-06-10 14:36:18 -0700

    added an exit command to the shell

---
+++ lib/andbug/cmd/exit.py
@@ -0,0 +1,23 @@
+## Copyright 2011, Scott W. Dunlop <swdunlop@gmail.com> All rights reserved.
+##
+## AndBug is free software: you can redistribute it and/or modify it under 
+## the terms of version 3 of the GNU Lesser General Public License as 
+## published by the Free Software Foundation.
+##
+## AndBug is distributed in the hope that it will be useful, but WITHOUT ANY
+## WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+## FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for 
+## more details.
+##
+## You should have received a copy of the GNU Lesser General Public License
+## along with AndBug.  If not, see <http://www.gnu.org/licenses/>.
+
+'implementation of the "stop" command'
+
+import andbug.command
+import os
+
+@andbug.command.action('', shell=True)
+def exit(name=None):
+    'terminates andbug with prejudice'
+    os._exit(0)
